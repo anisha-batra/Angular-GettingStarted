018ed508d7baf0b3a18e5d6d1ef04b11d7819273
Anisha Batra <anisha.batra11@gmail.com>
2018-06-27 16:24:53 -0700

    Created Product Compoent Page

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // specify the selector of the component being used as directive
+  template: `
+    <div> <h1> {{pageTitle}} </h1>
+    <pm-products></pm-products>
+  `
 })
 export class AppComponent {
   title = 'Angular: Getting Started';
